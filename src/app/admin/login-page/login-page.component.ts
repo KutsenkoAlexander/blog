673f2e7c28f04750cdb6cb13e96c3fd8a709b31d
alexander <kucenkoaleksandr@gmail.com>
2019-11-01 19:45:50 +0200

    adding route security

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
 import {User} from '../../shared/interfaces';
 import {AuthService} from '../shared/services/auth.service';
-import {Router} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-login-page',
@@ -13,13 +13,21 @@ export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
   submitted = false;
+  message: string;
 
-  constructor(public auth: AuthService, private router: Router) { }
+  constructor(public auth: AuthService,
+              private router: Router,
+              private activateRoute: ActivatedRoute) { }
 
   ngOnInit() {
     this.form = new FormGroup({
       email: new FormControl(null, [Validators.required, Validators.email]),
       password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+    });
+    this.activateRoute.queryParams.subscribe((params: Params) => {
+      if (params['logined']) {
+        this.message = 'Please, enter login and password';
+      }
     });
   }
 
