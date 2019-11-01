189c1b3eb8be1778074965f7305e2b57b8a12dd1
akut <akut@ciklum.com>
2019-11-01 16:37:39 +0200

    adding keeping token

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -12,6 +12,7 @@ import {Router} from '@angular/router';
 export class LoginPageComponent implements OnInit {
 
   form: FormGroup;
+  submitted = false;
 
   constructor(private auth: AuthService, private router: Router) { }
 
@@ -27,15 +28,16 @@ export class LoginPageComponent implements OnInit {
       return;
     }
 
+    this.submitted = true;
     const user: User = {
       email: this.form.value.email,
-      password: this.form.value.password,
-      returnSecureToken: true
+      password: this.form.value.password
     };
 
     this.auth.login(user).subscribe(() => {
       this.form.reset();
       this.router.navigate(['/admin', 'dashboard']);
+      this.submitted = false;
     });
 
     console.log(user);
