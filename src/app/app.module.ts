6ca7a8bb1202b115419de4ca9b304808472b81f5
alexander <kucenkoaleksandr@gmail.com>
2019-11-03 17:07:48 +0200

    adding localization

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,19 @@
 import {BrowserModule} from '@angular/platform-browser';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
 import {NgModule, Provider} from '@angular/core';
+import {registerLocaleData} from '@angular/common';
+import enLocale from '@angular/common/locales/en-ER';
 
 import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {MainLayoutComponent} from './shared/components/main-layout/main-layout.component';
 import {HomePageComponent} from './home-page/home-page.component';
 import {PostPageComponent} from './post-page/post-page.component';
-import {PostComponent} from './shared/components/post/post.component';
 import {SharedModule} from './shared/shared.module';
-import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {PostComponent} from './shared/components/post/post.component';
 import {AuthIterceptor} from './shared/auth.iterceptor';
+
+registerLocaleData(enLocale, 'en-ER');
 
 const AUTH_INTERCEPTOR_PROVIDER: Provider = {
   provide: HTTP_INTERCEPTORS,
